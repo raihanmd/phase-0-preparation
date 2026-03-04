57ac24b02752cb16849dc67c12a1bf3c8899f7be
raihanmd <mraihanf471@gmail.com>
2026-03-04 22:44:48 +0700

    finish: quiz 1, 2

---
+++ quiz/Quiz-Fcking-Number-Pattern/soal-2.ts
@@ -0,0 +1,97 @@
+/**
+  Square Number
+
+  Diberikan sebuah function squareNumber dimana menerima inputan berupa number.
+  Function ini akan mengembalikan array multidimensi yang isi value tersebut secara
+  proses menyerupai `board snakes and ladders` (angka 1 dimulai dari KOLOM ATAS) TAPI
+  alih-alih menuliskan value angka kamu akan menuliskan simbol/karakter dengan syarat sebagai berikut:
+  - jika value merupakan bilangan genap maka diganti menjadi karakter 'o'
+  - jika value merupakan bilangan ganjil maka diganti menjadi karakter 'x'
+  - jika value merupakan kelipatan 4 maka diganti menjadi simbol '#'
+
+  Contoh 1:
+  ==========
+  input: 3
+  proses:
+          [
+            [ 1, 2, 3 ],
+            [ 6, 5, 4 ],
+            [ 7, 8, 9 ]
+          ]
+  output:
+          [
+            [x, o, x],
+            [o, x, #],
+            [x, #, x]
+          ]
+
+  Contoh 2:
+  ==========
+  input: 4
+  proses:
+        [
+          [ 1, 2, 3, 4 ],
+          [ 8, 7, 6, 5 ],
+          [ 9, 10, 11, 12 ],
+          [ 16, 15, 14, 13 ]
+        ]
+  output:
+        [
+          [ x, o, x, # ],
+          [ #, x, o, x ],
+          [ x, o, x, # ],
+          [ #, x, o, x ]
+        ]
+NOTE:
+ - INPUT PARAMETER MINIMAL 3, JIKA KURANG DARI 3 MAKA RETURN 'Minimal input adalah 3'
+**/
+
+function squareNumber(num: number) {
+  const res: string[][] = [];
+  for (let i = 0; i < num; i++) {
+    res[i] = [];
+    for (let j = 0; j < num; j++) {
+      const normJ = j + 1;
+
+      const calc = i * num + normJ;
+
+      let val: string;
+
+      if (calc % 4 === 0) {
+        val = "#";
+      } else if (calc % 2 === 0) {
+        val = "o";
+      } else {
+        val = "x";
+      }
+
+      if (i % 2 !== 0) {
+        res[i][Math.abs(res[0].length - 1 - j)] = val;
+      } else {
+        res[i][j] = val;
+      }
+    }
+  }
+
+  return res;
+}
+
+console.log(squareNumber(3));
+// [ [x, o, x],  [o, x, #], [x, #, x] ]
+
+console.log(squareNumber(4));
+// [ [ x, o, x, # ],
+//   [ #, x, o, x ],
+//   [ x, o, x, # ],
+//   [ #, x, o, x ] ]
+
+console.log(squareNumber(5));
+// [
+//   [ x, o, x, #, x ],
+//   [ o, x, #, x, o ],
+//   [ x, o, x, #, x ],
+//   [ #, x, o, x, # ],
+//   [ x, o, x, #, x ]
+// ]
+
+console.log(squareNumber(2)); // Minimal input adalah 3
---
+++ quiz/Quiz-Fcking-Number-Pattern/soal-3.ts
@@ -0,0 +1,77 @@
+/**
+  Square Number
+  Diberikan sebuah function squareNumber2 dimana menerima inputan berupa angka.
+  Function ini akan mengembalikan array multidimensi angka x angka yang isi value
+  dari array multidimensi tersebut menyerupai `board snakes and ladders`
+  Contoh 1:
+  input: 3
+  output:
+      [
+        [7, 8, 9],
+        [6, 5, 4],
+        [1, 2, 3]
+      ]
+  Contoh 2:
+  input: 4
+  output:
+      [
+        [ 16, 15, 14, 13 ],
+        [ 9, 10, 11, 12 ],
+        [ 8, 7, 6, 5 ],
+        [ 1, 2, 3, 4 ]
+      ]
+NOTE:
+ - INPUT PARAMETER MINIMAL 3, JIKA KURANG DARI 3 MAKA RETURN 'Minimal input adalah 3'
+ - NILAI FULL(100) AKAN DIBERIKAN JIKA BERHASIL MENYELESAIKAN KASUS DIATAS
+ - NILAI 80 AKAN DIBERIKAN JIKA value angka di dalam array tersebut tidak persis
+   menyerupai snakes and ladders TAPI angka 1 harus tetap berada di kolom paling bawah!
+   CONTOH:
+     [
+       [ 13, 14, 15, 16 ],
+       [ 9, 10, 11, 12 ],
+       [ 5, 6, 7, 8 ],
+       [ 1, 2, 3, 4 ]
+     ]
+**/
+
+function squareNumber2(num: number) {
+  const res: number[][] = Array.from({ length: num }).map((_) => []);
+  let counter = 1;
+
+  for (let i = num - 1; i >= 0; i--) {
+    // 3 - 1 - 2 => 0
+    // 3 - 1 - 1 => 1
+    // 3 - 1 - 0 => 2
+    const startCol = num - 1 - i;
+
+    if (startCol % 2 == 0) {
+      for (let j = 0; j < num; j++) {
+        res[i][j] = counter++;
+      }
+    } else {
+      for (let j = num - 1; j >= 0; j--) {
+        res[i][j] = counter++;
+      }
+    }
+  }
+
+  return res;
+}
+
+console.log(squareNumber2(3));
+// [ [ 7, 8, 9 ], [ 6, 5, 4 ], [ 1, 2, 3 ] ]
+
+console.log(squareNumber2(4));
+// [ [ 16, 15, 14, 13 ],
+//   [ 9, 10, 11, 12 ],
+//   [ 8, 7, 6, 5 ],
+//   [ 1, 2, 3, 4 ] ]
+
+console.log(squareNumber2(5));
+// [ [ 21, 22, 23, 24, 25 ],
+//   [ 20, 19, 18, 17, 16 ],
+//   [ 11, 12, 13, 14, 15 ],
+//   [ 10, 9, 8, 7, 6 ],
+//   [ 1, 2, 3, 4, 5 ] ]
+
+console.log(squareNumber2(2)); // Minimal input adalah 3
